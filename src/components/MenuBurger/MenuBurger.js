7d894d4ee45b3a4d6da82fac256714b837faea6c
szymonqqaz <szymonqqaz22@gmail.com>
2019-04-26 20:54:58 +0200

    media query for landscape

--- src/components/MenuBurger/MenuBurger.js
+++ src/components/MenuBurger/MenuBurger.js
@@ -73,10 +73,7 @@ const H1 = styled.h1`
   z-index: 2000;
   margin-right: 10%;
 
-  ${media.tablet`
-    margin-right: 45%;
-  `}
-
+  
   transition: transform .2s .3s;
   transform: translateX(${({visibleMenu}) => visibleMenu ? 0 : -200}%);
   transform: translateX(${({a}) => a ? 0 : null}%);
